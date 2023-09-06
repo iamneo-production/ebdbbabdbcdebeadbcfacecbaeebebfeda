d2b0cfc409d6a62d316aa63c0208fc8cac68ca6d
Iamneo <techadmin@iamneo.ai>
2023-09-06 16:46:08 +0000

    Backup Commit

--- angularapp/src/app/food-search.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FoodSearchPipe } from './food-search.pipe';
-
-describe('FoodSearchPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FoodSearchPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
